c2ccf04b304f48e5a54a3d64392fd3ccc0087fdc
Rey <reyxeka@gmail.com>
2023-05-23 19:12:42 -0400

    Finished front-end refactoring and styling

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,61 @@
-window.addEventListener('DOMContentLoaded', () => {
+function createCard(name, location, description, pictureUrl, startFormat, endFormat) {
+    return `
+      <div class="col-4 p-2">
+        <div class="card shadow">
+            <img src="${pictureUrl}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted b-3">${location}</h6>
+                <p class="card-text">${description}</p>
+            </div>
+            <div class="card-footer">
+                ${startFormat} - ${endFormat}
+            </div>
+        </div>
+      </div>
+    `;
+}
 
+window.addEventListener('DOMContentLoaded', async () => {
+
+    const url = 'http://localhost:8000/api/conferences/';
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        // Figure out what to do when the response is bad
+        alert(response.statusText)
+      } else {
+        const data = await response.json();
+
+        for (let conference of data.conferences) {
+          const detailUrl = `http://localhost:8000${conference.href}`;
+          const detailResponse = await fetch(detailUrl);
+          if (detailResponse.ok) {
+            const details = await detailResponse.json();
+
+            const title = details.conference.name;
+            const location = details.conference.location.name;
+            const description = details.conference.description;
+            const pictureUrl = details.conference.location.picture_url;
+
+            const startDate = new Date(details.conference.starts);
+            const startFormat = `${startDate.getMonth()}/${startDate.getDate()}/${startDate.getFullYear()}`;
+            const endDate = new Date(details.conference.ends);
+            const endFormat = `${endDate.getMonth()}/${endDate.getDate()}/${endDate.getFullYear()}`;
+
+            const html = createCard(title, location, description, pictureUrl, startFormat, endFormat);
+            const column = document.querySelector('.row');
+            column.innerHTML += html;
+
+          }
+        }
+
+      }
+    } catch (e) {
+      // Figure out what to do if an error is raised
+      alert(response.statusText)
+    }
 
 });
